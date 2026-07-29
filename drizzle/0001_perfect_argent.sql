8abb17196da7830efc785209f18b99897d6d1fc5
Tim <tim@timsexperiments.foo>
2026-07-29 07:52:18 -0500

    chore: collapse database migrations to baseline

--- drizzle/0001_perfect_argent.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `config` ADD `otel_endpoint` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE `config` ADD `otel_headers` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE `config` ADD `otel_sample_ratio` integer DEFAULT 100 NOT NULL;
